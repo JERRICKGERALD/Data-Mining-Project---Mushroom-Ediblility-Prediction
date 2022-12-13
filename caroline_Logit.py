02e4e564f66d5f831d3abb18d17cce2a83cb1bdc
ctkrall <89491139+ctkrall@users.noreply.github.com>
2022-12-12 19:59:01 -0500

    Final version: logistic model fitting

---
+++ caroline_Logit.py
@@ -0,0 +1,66 @@
+#%%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("final.csv")
+print(df.head())
+#%%
+#need to encode all strings to floats
+from sklearn import preprocessing
+
+le = preprocessing.LabelEncoder()
+for column_name in df.columns:
+        if df[column_name].dtype == object:
+            df[column_name] = le.fit_transform(df[column_name])
+        else:
+            pass
+
+print(df.head())
+# NOTE: class p = 1
+
+# %%
+# based on the model summary, I will drop gill attachment and stem color from the model
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+y = df[['class']]
+x = df[['ring-type', 'stem-width', 'cap-shape', 'gill-color', 'stem-height']]
+
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.30, random_state=123)
+fullLogit = LogisticRegression() #initiate logit model
+
+fullLogit.fit(xtrain, ytrain)
+
+
+#%%
+# model score and classification report
+from sklearn.metrics import classification_report
+print(fullLogit.score(xtest, ytest))
+print(fullLogit.score(xtrain, ytrain))
+
+ytrue, ypred = ytest, fullLogit.predict(xtest)
+print(classification_report(ytrue, ypred))
+
+# %%
+# Confusion Matrix
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+conf = confusion_matrix(ytrue, ypred)
+ConfusionMatrixDisplay.from_predictions(ytrue, ypred)
+
+# %%
+# Model Evaluation metrics
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score, roc_auc_score
+
+print(precision_score(ytest, ypred))
+print(recall_score(ytest, ypred))
+print(accuracy_score(ytest, ypred))
+print('F1 Score: %.3f' % f1_score(ytest, ypred))
+print(roc_auc_score(ytest, ypred))
+# %%
+# ROC Curve chart
+from sklearn.metrics import RocCurveDisplay
+RocCurveDisplay.from_predictions(ytest, ypred)
+
+
+# %%
