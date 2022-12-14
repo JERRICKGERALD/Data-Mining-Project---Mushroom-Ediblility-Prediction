bbe6c1ae33c1b773244549566e8a18d3b1800618
ctkrall <89491139+ctkrall@users.noreply.github.com>
2022-12-14 16:55:40 -0500

    Final logit file

--- caroline_Logit.py
+++ caroline_Logit.py
@@ -23,7 +23,44 @@ print(df.head())
 # based on the model summary, I will drop gill attachment and stem color from the model
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+yf = df[['class']]
+xf = df[['ring-type', 'stem-width', 'cap-shape', 'gill-color', 'stem-height', 'stem-color', 'gill-attachment']]
 
+xftrain, xftest, yftrain, yftest = train_test_split(xf, yf, test_size=0.30, random_state=123)
+fullLogit = LogisticRegression() #initiate logit model
+
+fullLogit.fit(xftrain, yftrain)
+
+#%%
+from sklearn.metrics import classification_report
+print(fullLogit.score(xftest, yftest))
+print(fullLogit.score(xftrain, yftrain))
+
+yftrue, yfpred = yftest, fullLogit.predict(xftest)
+print(classification_report(yftrue, yfpred))
+
+# %%
+# Confusion Matrix
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+conf = confusion_matrix(yftrue, yfpred)
+ConfusionMatrixDisplay.from_predictions(yftrue, yfpred)
+
+# %%
+# Model Evaluation metrics
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score, roc_auc_score
+
+print(precision_score(yftest, yfpred))
+print(recall_score(yftest, yfpred))
+print(accuracy_score(yftest, yfpred))
+print('F1 Score: %.3f' % f1_score(yftest, yfpred))
+print(roc_auc_score(yftest, yfpred))
+# %%
+# ROC Curve chart
+from sklearn.metrics import RocCurveDisplay
+RocCurveDisplay.from_predictions(yftest, yfpred)
+
+#%%
+# reduced model
 y = df[['class']]
 x = df[['ring-type', 'stem-width', 'cap-shape', 'gill-color', 'stem-height']]
 
